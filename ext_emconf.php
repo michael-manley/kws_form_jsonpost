ea2534fe159a656ea0f0a9cca2fbbd1c9d7b907f
Michael Manley <mjmanley@wisegs.net>
2025-12-02 15:39:25 -0800

    Modify author and update TYPO3 version constraints
    Updated author information and TYPO3 version constraints.

--- ext_emconf.php
+++ ext_emconf.php
@@ -4,15 +4,15 @@ $EM_CONF[$_EXTKEY] = [
     'title' => 'bkindler Form POST JSON Finisher',
     'description' => 'Provides HTTP JSON POST finisher for EXT:form',
     'category' => 'misc',
-    'author' => 'Björn Kindler',
+    'author' => 'Michael Manley; Björn Kindler',
     'author_email' => 'info@kindler-webservices.de',
     'state' => 'stable',
     'version' => '1.0.1',
     'constraints' => [
         'depends' =>
             [
-                'typo3' => '11.5.0-12.4.99',
-                'form' => '11.5.0-12.4.99',
+                'typo3' => '11.5.0-13.4.99',
+                'form' => '11.5.0-13.4.99',
             ],
         'conflicts' => [],
         'suggests' => [],
